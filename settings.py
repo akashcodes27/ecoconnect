297a00d1be33f6a6665358d0a62834900e3e39ea
Akash Patil <akashpatil2002@gmail.com>
2025-07-14 09:25:04 -0400

    Setting up files for the django project ecoconnect

--- settings.py
+++ settings.py
@@ -116,6 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Session and authentication settings 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
